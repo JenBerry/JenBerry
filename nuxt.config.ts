f5eea839c65b7b52e0cdc931ca06cf90fe90dfa2
Jen Berry <jenberrymail@gmail.com>
2026-03-07 13:36:45 +0000

    add umami analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["vuetify-nuxt-module", "@vueuse/nuxt"],
+  modules: ["vuetify-nuxt-module", "@vueuse/nuxt", "nuxt-umami"],
   css: ["~/assets/styles/global.scss"],
+  umami: {
+    id: "f30f3a35-6820-419b-ba98-7902dbce9baa",
+    host: "https://cloud.umami.is",
+    domains: ["jjmb.dev"],
+    urlOptions: {
+      trailingSlash: "never",
+      excludeHash: true,
+    },
+  },
   vuetify: {
     vuetifyOptions: {
       theme: {
